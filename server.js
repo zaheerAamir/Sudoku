5cfbd8a3c36f12ef46c1070b39dcbc64b304591c
zaheerAamir <aamirzaheer95@gmail.com>
2022-09-14 19:57:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,33 @@ app.use(express.json())
 
 
 app.post('https://project-sathom.herokuapp.com/', (req,res) => {
+
     const options = {
+        method: 'POST',
+        url: 'https://solve-sudoku.p.rapidapi.com/',
+        headers: {
+          'content-type': 'application/json',
+          'X-RapidAPI-Key': '4c950a4ca5msh959e74b886a4c78p1c992bjsnd97091aca10c',
+          'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
+        },
+        data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}'
+      };
+      
+      axios.request(options).then(function (response) {
+          console.log(response.data);
+      }).catch(function (error) {
+          console.error(error);
+      });
+
+
+
+
+
+
+
+
+
+   /*  const options = {
         method: 'POST',
         url: 'https://solve-sudoku.p.rapidapi.com/',
         headers: {
@@ -31,7 +57,7 @@ app.post('https://project-sathom.herokuapp.com/', (req,res) => {
         res.json(response.data)
     }).catch(function (error) {
 	    console.error(error)
-    })
+    }) */
 })
 
 app.listen(PORT, () => console.log('server listening on PORT ',{PORT}))
