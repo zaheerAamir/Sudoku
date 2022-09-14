6c204069184fc446e47595cb323c582641f421ce
zaheerAamir <aamirzaheer95@gmail.com>
2022-09-14 14:46:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.get('/',(req,res) => {
+    res.send('App is running')
+})
+
 app.post('/solve', (req,res) => {
     const options = {
         method: 'POST',
