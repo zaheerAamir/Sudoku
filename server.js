5629547a47d8617767d6b6fe27c167de86f48ee1
zaheerAamir <aamirzaheer95@gmail.com>
2022-09-14 20:04:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -20,31 +20,6 @@ app.post('https://project-sathom.herokuapp.com/', (req,res) => {
         method: 'POST',
         url: 'https://solve-sudoku.p.rapidapi.com/',
         headers: {
-          'content-type': 'application/json',
-          'X-RapidAPI-Key': '4c950a4ca5msh959e74b886a4c78p1c992bjsnd97091aca10c',
-          'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
-        },
-        data: '{"puzzle":"2.............62....1....7...6..8...3...9...7...6..4...4....8....52.............3"}'
-      };
-      
-      axios.request(options).then(function (response) {
-          console.log(response.data);
-      }).catch(function (error) {
-          console.error(error);
-      });
-
-
-
-
-
-
-
-
-
-   /*  const options = {
-        method: 'POST',
-        url: 'https://solve-sudoku.p.rapidapi.com/',
-        headers: {
             'content-type': 'application/json',
             'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
@@ -57,7 +32,7 @@ app.post('https://project-sathom.herokuapp.com/', (req,res) => {
         res.json(response.data)
     }).catch(function (error) {
 	    console.error(error)
-    }) */
+    })
 })
 
 app.listen(PORT, () => console.log('server listening on PORT ',{PORT}))
