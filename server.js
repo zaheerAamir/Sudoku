fc41fcd1518a9ca4f9e63e581eb2f0c75b337081
zaheerAamir <aamirzaheer95@gmail.com>
2022-09-15 20:48:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 app.use(cors({
-    origin: 'https://https://project-sathom.herokuapp.com/:heartfelt-blancmange-4ce36d.netlify.app/',
+    origin: 'https://project-sathom.herokuapp.com/:heartfelt-blancmange-4ce36d.netlify.app/',
     methods: ['GET','POST'],
 }))
 app.use(express.json())
