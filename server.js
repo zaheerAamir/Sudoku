68ff3e18eff7da0f14de8dc9b85e88ccc66ac927
zaheerAamir <aamirzaheer95@gmail.com>
2023-12-06 22:11:02 +0530

    Updated Packages..

--- server.js
+++ server.js
@@ -2,21 +2,22 @@ const PORT = process.env.PORT || 8000
 const axios = require('axios').default
 const express = require('express')
 const cors = require('cors')
-const serverless = require('serverless-http')
+const status = require('express-status-monitor')
 
 require('dotenv').config()
 const app = express()
-const router = express.Router()
 
-app.use(cors({origin: '*'}))
+app.use(status())
+app.use(cors({ origin: '*' }))
 
 app.use(express.json())
 
 /* app.get('/',(req,res) => {
     res.send('App is running')
 }) */
-app.post('/', (req,res) => {
+app.post('/', (req, res) => {
 
+    const tick = performance.now()
     const options = {
         method: 'POST',
         url: 'https://solve-sudoku.p.rapidapi.com/',
@@ -25,20 +26,21 @@ app.post('/', (req,res) => {
             'X-RapidAPI-Key': process.env.RAPID_API_KEY,
             'X-RapidAPI-Host': 'solve-sudoku.p.rapidapi.com'
         },
-        data: {puzzle: req.body.numbers}     //req.body.numbers
+        data: { puzzle: req.body.numbers }     //req.body.numbers
     }
 
     axios.request(options).then(function (response) {
-	    console.log(response.data);
+        console.log(response.data);
         console.log(response.status)
+        const tock = performance.now()
+        console.log(`Time took:  ${tock - tick}ms `)
         res.json(response.data)
-        res.json('yooooooo')
     }).catch(function (error) {
-	    console.error(error)
+        console.error(error)
     })
 
 
 })
 
 
-app.listen(PORT, () => {console.log(`Server is running on ${PORT}`)})
+app.listen(PORT, () => { console.log(`Server is running on ${PORT}`) })
